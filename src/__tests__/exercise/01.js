cac294f64b5d0a1b35b7a62e0301ef9d9de7cb13
marioperez25 <mariofph@gmail.com>
2021-03-24 17:57:17 -0600

    simple test with react DOM

--- src/__tests__/exercise/01.js
+++ src/__tests__/exercise/01.js
@@ -6,24 +6,38 @@ import ReactDOM from 'react-dom'
 import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  // 🐨 create a div to render your component to (💰 document.createElement)
-  //
-  // 🐨 append the div to document.body (💰 document.body.append)
-  //
-  // 🐨 use ReactDOM.render to render the <Counter /> to the div
-  // 🐨 get a reference to the increment and decrement buttons:
-  //   💰 div.querySelectorAll('button')
-  // 🐨 get a reference to the message div:
-  //   💰 div.firstChild.querySelector('div')
-  //
-  // 🐨 expect the message.textContent toBe 'Current count: 0'
-  // 🐨 click the increment button (💰 increment.click())
-  // 🐨 assert the message.textContent
-  // 🐨 click the decrement button (💰 decrement.click())
-  // 🐨 assert the message.textContent
-  //
-  // 🐨 cleanup by removing the div from the page (💰 div.remove())
-  // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
+
+  const div = document.createElement('div') 
+  document.body.append(div)
+
+  ReactDOM.render(<Counter/> , div)
+
+  const message = div.firstChild.querySelector('div')
+  const [decrement, increment] = div.querySelectorAll('button')
+  // use MouseEvent to generate more accurate event
+  const clickEvent = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    button: 0
+  })
+  const countMessage = 'Current count:'
+
+  increment.dispatchEvent(clickEvent)
+  expect(message.textContent).toBe(`${countMessage} 1`)
+
+  increment.dispatchEvent(clickEvent)
+  expect(message.textContent).toBe(`${countMessage} 2`)
+
+  decrement.dispatchEvent(clickEvent)
+  expect(message.textContent).toBe(`${countMessage} 1`)
+
+  decrement.dispatchEvent(clickEvent)
+  expect(message.textContent).toBe(`${countMessage} 0`)
+
+  decrement.dispatchEvent(clickEvent)
+  expect(message.textContent).toBe(`${countMessage} -1`)
+
+  div.remove() // cleanup
 })
 
 /* eslint no-unused-vars:0 */
